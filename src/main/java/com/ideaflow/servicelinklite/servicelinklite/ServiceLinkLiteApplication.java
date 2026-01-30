5149ac6bc12087af3ad98e8909bc4e5fa6b22353
biliww <843145936@qq.com>
2026-01-30 16:34:09 +0800

    初始化

---
+++ src/main/java/com/ideaflow/servicelinklite/servicelinklite/ServiceLinkLiteApplication.java
@@ -0,0 +1,13 @@
+package com.ideaflow.servicelinklite.servicelinklite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceLinkLiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceLinkLiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ideaflow/servicelinklite/servicelinklite/ServiceLinkLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideaflow.servicelinklite.servicelinklite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceLinkLiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
